6b577e6fe7ec7a345108eb6a7e320632e758ba4c
Fiarzen <jeremylam1995@gmail.com>
2025-12-04 15:20:57 +0000

    day2 part2

---
+++ day-2/part-2-script.py
@@ -0,0 +1,31 @@
+def find_subtotal_invalid_ids_in_range(range_str):
+    start_str, end_str = range_str.split("-")
+    start, end = int(start_str), int(end_str)
+    subtotal = 0
+    for i in range(start, end + 1):
+        s = str(i)
+        res = s in (s + s)[1:-1]
+        if res:
+            subtotal += i
+        
+
+    return subtotal
+
+def add_all_invalid_ids(input_file):   
+    with open(input_file, "r") as file:
+        line = file.readline().strip()
+    ranges = line.split(",")
+    sum_of_invalid_ids = 0
+    for range in ranges:
+        sum_of_invalid_ids += find_subtotal_invalid_ids_in_range(range)
+    return sum_of_invalid_ids
+
+def main():
+    input = "./day-2/input.txt"
+    result = add_all_invalid_ids(input)
+    print(f"Sum of all invalid IDs: {result}")
+
+if __name__ == "__main__":
+    main()
+
+
